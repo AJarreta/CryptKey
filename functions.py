eb36a1e9d600cd936bbc5b80c8aa9b76bf7ace02
Antonio Jarreta <antoniojarreta@gmail.com>
2016-08-11 14:35:39 +0100

    Added Resolve and Reset functions

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 class EncryptedText(object):
 
-    WorkingCopyEncText = []
-    ReturnedEncryptedText = ''
+    WorkingCopyText = []
+    FunctionResult = ''
     ExceptionList = {}
 
     def __init__(self, UserInput, EncryptionKey):
@@ -19,11 +19,43 @@ class EncryptedText(object):
                     CurrentEncryptedChar = str(unichr(127))
                     self.ExceptionList.update({index: CurrentIntChar})
                     break
-            self.WorkingCopyEncText.append(CurrentEncryptedChar)
+            self.WorkingCopyText.append(CurrentEncryptedChar)
             print "The letter \"", i, "\"with the integer value of", CurrentIntChar, \
                 "was encrypted into \"", CurrentEncryptedChar, \
                 "\"that has the integer value", ord(CurrentEncryptedChar), \
                 "and has the variable type", type(CurrentEncryptedChar)
-        self.ReturnedEncryptedText = ''.join(self.WorkingCopyEncText)
+        self.FunctionResult = ''.join(self.WorkingCopyText)
 
     def Resolve_Text(self):
+        for index, i in enumerate(self.FunctionResult, start=0):
+            if index in self.ExceptionList:
+                CurrentIntChar = ord(self.ExceptionList[index])
+            else:
+                CurrentIntChar = ord(i)
+            while True:
+                try:
+                    CurrentRestoredChar = str(unichr(CurrentIntChar ^ self.EncriptionKey))
+                    break
+                except UnicodeEncodeError:
+                    CurrentRestoredChar = "X"
+                    break
+            self.WorkingCopyText.append(CurrentRestoredChar)
+            print "The letter \"", i, "\"with the integer value of", CurrentIntChar, \
+                "was de-encrypted to \"", CurrentRestoredChar, \
+                "\"that has the integer value", ord(CurrentRestoredChar), \
+                "and has the variable type", type(CurrentRestoredChar)
+        self.FunctionResult = ''.join(self.WorkingCopyText)
+
+    def Reset_Values(self):
+        self.WorkingCopyText = []
+        self.FunctionResult = ''
+        self.ExceptionList = {}
+
+    def Reset_Exceptions(self):
+        self.ExceptionList = {}
+
+    def Reset_WorkingCopy(self):
+        self.WorkingCopyText = []
+
+    def Reset_Result:
+        self.FunctionResult = ''
